d4af0dc52ba6e6a728636ff80e0128a68b1218b6
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2022-07-28 14:40:27 +0400

    admin fields

--- example/admin.py
+++ example/admin.py
@@ -9,6 +9,9 @@ class Admin(ModelAdmin):
     """Admin site customization."""
 
     list_display = ['name', 'lon', 'lat']
+    geomap_field_longitude = "id_lon"
+    geomap_field_latitude = "id_lat"
+    search_fields = ['name']
 
 
 admin.site.register(Location, Admin)
