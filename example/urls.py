75e5447c43bd8ac242897fa90dff810a7b21e907
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2022-07-29 11:31:15 +0400

    Django 4 support (#14)

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,9 @@
 """Django url router."""
-from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
 from .views import home
 
 urlpatterns = [  # pylint: disable=invalid-name
-  url(r'^$', home, name='home'),
-  url(r'^admin/', admin.site.urls),
+  path('', home, name='home'),
+  path('admin/', admin.site.urls),
 ]
