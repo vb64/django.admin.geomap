4597fdf13082237b00aad163f969879e017349ba
vb64 <vit.sar68@gmail.com>
2023-06-28 10:36:23 +0400

    Admin site for uuid model

--- example/admin.py
+++ example/admin.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django_admin_geomap import ModelAdmin
 
-from .models import Location
+from .models import Location, LocationUuid
 
 
 class Admin(ModelAdmin):
@@ -15,4 +15,17 @@ class Admin(ModelAdmin):
     geomap_item_zoom  = "10"
     geomap_autozoom = geomap_item_zoom
 
+
+class AdminUuid(Admin):
+    """Admin site for uuid model."""
+
+    list_display = ['name', 'longitude', 'latitude']
+    geomap_field_longitude = "id_longitude"
+    geomap_field_latitude = "id_latitude"
+    search_fields = ['name']
+    geomap_item_zoom  = "10"
+    geomap_autozoom = geomap_item_zoom
+
+
 admin.site.register(Location, Admin)
+admin.site.register(LocationUuid, AdminUuid)
